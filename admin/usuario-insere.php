277e0a4f9c4a6108549a3bb9b309d4eecc4019fd
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-05-29 16:58:26 -0300

    Update usuario-insere.php

--- admin/usuario-insere.php
+++ admin/usuario-insere.php
@@ -5,7 +5,20 @@ require_once "../inc/funcoes-usuarios.php";
 
 //Codigo para detectar se o botão 'INSERIR' for ativado
 if(isset($_POST['inserir'])){
-	echo "OK";
+	//Aqui colocaremos a captura dos dados inseridos no campos do formulário
+// variável  -  requerimento do formulário(POST ou GET)  - campo do formulário      
+	$nome              = $_POST                                ['nome'];
+	$email             = $_POST                                ['email'];
+	$tipo              = $_POST                                ['tipo'];
+
+	//Capturando e criptografando a senha
+	$senha             = password_hash ($_POST ['senha'], PASSWORD_DEFAULT);
+
+	//Chamar a função para ela receber os parâmetros
+	inserirUsuario($conexao, $nome, $email, $tipo, $senha);
+
+	//Redirecionando para a lista de usuários
+	header("location:usuarios.php");
 }
 
 ?>
